a72c1b7fe186cb05f5eb1897c2eac797c8fd332a
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2023-11-21 16:29:12 +0000

    npm install cors and backend code for update

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,16 +1,18 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+app.use(cors())
+
 const generateId = () => {
-    let random = Math.floor(Math.random() * 1000);
+  let random = Math.floor(Math.random() * 1000);
   return random
 }
 
@@ -22,31 +24,31 @@ const year = currentDate.getFullYear();
 const time = currentDate.toLocaleTimeString();
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456",
-      "favorite": false
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "favorite": false
       
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523",
-      "favorite": false
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345",
-      "favorite": false
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "favorite": false
-    }
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "favorite": false
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "favorite": false
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "favorite": false
+  }
 ]
 
 //FETCHES "HOME" PAGE
@@ -56,10 +58,10 @@ app.get('/', (request, response) => {
 
 //FETCHES INFO PAGE
 app.get('/info', (request, response) => {
-    response.send(`
-        <p>Phonebook has info for ${persons.length} people</p> 
-        <p>${weekday} ${month} ${dayOfMonth} ${year} ${time}</p>
-    `)
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p> 
+    <p>${weekday} ${month} ${dayOfMonth} ${year} ${time}</p>
+  `)
 })
 
 //FETCHES ALL RESOURCES
@@ -69,52 +71,64 @@ app.get('/api/persons', (request, response) => {
 
 //FETCHES IDENTIFIED RESOURCE
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
 
-    if(person){
-        response.json(person)
-    }
-    else{
-        response.sendStatus(404)
-    }
+  if(person){
+    response.json(person)
+  }
+  else{
+    response.sendStatus(404)
+  }
 })
 
 //CREATES NEW RESOURCE
 app.post('/api/persons', (request, response) => {
-    const body = request.body
-    const nameExists = persons.some(person => person.name === body.name)
-    const phoneExists = persons.some(person => person.number === body.number)
+  const body = request.body
+  const nameExists = persons.some(person => person.name === body.name)
+  const phoneExists = persons.some(person => person.number === body.number)
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({ 
-        error: 'content missing' 
-        })
-    }
-    else if (nameExists || phoneExists) {
-        return response.status(400).json({
-        error: 'name and phone must be unique'
-        })
-    }
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+    error: 'content missing' 
+    })
+  }
+  else if (nameExists || phoneExists) {
+    return response.status(400).json({
+    error: 'name and phone must be unique'
+    })
+  }
 
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-        favorite: body.important || false
-    }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+    favorite: body.important || false
+  }
 
-    persons = persons.concat(person)
+  persons = persons.concat(person)
 
-    response.json(person)
+  response.json(person)
+})
+app.put('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const personIndex = persons.findIndex(p => p.id === id);
+
+  if (personIndex !== -1) {
+    persons[personIndex].favorite = !persons[personIndex].favorite;
+    response.json(persons[personIndex]);
+  } 
+  else {
+    response.status(404).json({ error: 'Person not found' });
+  }
 })
 
 //DELETES IDENTIFIED RESOURCE
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
 
-    response.sendStatus(204)
+  response.sendStatus(204)
 })
 
 const PORT = 3001
