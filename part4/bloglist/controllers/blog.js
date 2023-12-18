855e3ba4073c5d7b14eadab9235864316f75dccd
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2023-12-18 12:07:30 +0000

    added put and delete requests

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -25,4 +25,28 @@ blogRouter.post('/', (request, response, next) => {
     .catch(error => next(error))
 })
 
+//DELETES IDENTIFIED RESOURCE
+blogRouter.delete('/:id', async (request, response, next) => {
+  await Blog.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+//UPDATE IDENTIFIED RESOURCE
+blogRouter.put('/:id', async (request, response, next) => {
+  const { author, title, url, upvotes } = request.body
+
+  await Blog.findByIdAndUpdate(
+    request.params.id, 
+    { author, title, url, upvotes },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+    .catch(error => next(error))
+})
+
 module.exports = blogRouter
